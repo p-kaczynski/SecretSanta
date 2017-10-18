913027aa80c8f09e7feb645a66c946d09c6d1ae0
Paweł Kaczyński <p.a.kaczynski@gmail.com>
2017-10-18 23:39:18 +0100

    Fixes #4 Added help text and "required" asterisk.

---
+++ SecretSanta/App_Code/HelpTextHtmlExtension.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using SecretSanta.Models.Attributes;
+
+namespace SecretSanta
+{
+    public static class HelpTextHtmlExtension
+    {
+        public static IHtmlString HelpTextFor<TModel, TProperty>(
+            this HtmlHelper<TModel> html,
+            Expression<Func<TModel, TProperty>> ex
+        )
+        {
+            var metadata = ModelMetadata.FromLambdaExpression(ex, html.ViewData);
+            var helpTextBuilder = new StringBuilder();
+            if (metadata.ModelType.IsValueType ? metadata.ModelType.GetCustomAttributes(typeof(RequiredAttribute), false).Any() : metadata.IsRequired)
+            {
+                helpTextBuilder.Append('*');
+            }
+            if (metadata.AdditionalValues.ContainsKey(HelpTextAttribute.HelpTextPropertyName))
+            {
+                helpTextBuilder.Append(metadata.AdditionalValues[HelpTextAttribute.HelpTextPropertyName] as string);
+            }
+            if (helpTextBuilder.Length == 0)
+                return MvcHtmlString.Empty;
+
+            var helpTextHtml = new TagBuilder("p");
+            helpTextHtml.Attributes.Add("class", "form-text text-muted");
+            helpTextHtml.InnerHtml = helpTextBuilder.ToString();
+
+            return new MvcHtmlString(helpTextHtml.ToString());
+
+        }
+    }
+}
---
+++ SecretSanta/Models/Attributes/HelpTextAttribute.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Reflection;
+using System.Web.Mvc;
+
+namespace SecretSanta.Models.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class HelpTextAttribute : Attribute, IMetadataAware
+    {
+        public const string HelpTextPropertyName = "custom.help_text";
+        private readonly string _helpText;
+        public HelpTextAttribute(string helpText)
+        {
+            _helpText = helpText;
+        }
+
+        public HelpTextAttribute(Type resourceType, string propertyName)
+        {
+            var property = resourceType.GetProperty(propertyName, BindingFlags.Public | BindingFlags.Static | BindingFlags.NonPublic);
+            if (property != null && property.GetMethod != null)
+                _helpText = property.GetValue(null) as string;
+        }
+
+        public void OnMetadataCreated(ModelMetadata metadata)
+        {
+            metadata.AdditionalValues[HelpTextPropertyName] = _helpText;
+        }
+    }
+}
