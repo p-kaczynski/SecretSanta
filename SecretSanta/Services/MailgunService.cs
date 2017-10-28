1c2be80f4ef64717a072635518014aa0254b1ad6
Paweł Kaczyński <p.a.kaczynski@gmail.com>
2017-10-29 00:03:00 +0100

    Fixes #12 -  added mailgun support

---
+++ SecretSanta/Services/MailgunService.cs
@@ -0,0 +1,46 @@
+﻿using System.Net;
+using NLog;
+using RestSharp;
+using RestSharp.Authenticators;
+using SecretSanta.Common.Interface;
+
+namespace SecretSanta.Services
+{
+    public class MailgunService
+    {
+        private static readonly Logger Log = LogManager.GetCurrentClassLogger();
+
+        private readonly IConfigProvider _configProvider;
+        private const string SendMessageEndpoint = "messages";
+
+        public MailgunService(IConfigProvider configProvider)
+        {
+            _configProvider = configProvider;
+        }
+
+        public bool SendEmail(string to, string subject, string body)
+        {
+            var client = new RestClient(_configProvider.MailgunBaseDomain);
+            client.Authenticator =
+                new HttpBasicAuthenticator("api",
+                    _configProvider.MailgunApiKey);
+
+            var request = new RestRequest(SendMessageEndpoint, Method.POST);
+            request.AddParameter("from", _configProvider.MailgunFrom);
+            request.AddParameter("to", to);
+            request.AddParameter("subject", subject);
+            request.AddParameter("text", body);
+            
+            var result = client.Execute(request);
+            Log.Trace($"Mailgun response ({result.StatusCode}): {result.Content}");
+            switch (result.StatusCode)
+            {
+                case HttpStatusCode.OK:
+                    return true;
+                default:
+                    Log.Error($"Cannot send a message to {to} - got {result.StatusCode} back. Response: {result.Content}");
+                    return false;
+            }
+        }
+    }
+}
