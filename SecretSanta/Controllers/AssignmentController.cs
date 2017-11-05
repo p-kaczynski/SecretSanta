c6cdba91d2debb19170018d250e06cb30502f9ee
Paweł Kaczyński <p.a.kaczynski@gmail.com>
2017-11-05 14:59:28 +0000

    Fixes #30 - sending emails after assignment

--- SecretSanta/Controllers/AssignmentController.cs
+++ SecretSanta/Controllers/AssignmentController.cs
@@ -1,6 +1,11 @@
-﻿using System.Web.Mvc;
+﻿using System.Collections.Concurrent;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Hosting;
+using System.Web.Mvc;
 using SecretSanta.Common.Interface;
 using SecretSanta.Common.Result;
+using SecretSanta.Models;
 using SecretSanta.Security;
 
 namespace SecretSanta.Controllers
@@ -9,10 +14,12 @@ namespace SecretSanta.Controllers
     public class AssignmentController : BaseController
     {
         private readonly IUserRepository _userRepository;
+        private readonly IEmailService _emailService;
 
-        public AssignmentController(IUserRepository userRepository)
+        public AssignmentController(IUserRepository userRepository, IEmailService emailService)
         {
             _userRepository = userRepository;
+            _emailService = emailService;
         }
 
         [HttpGet]
@@ -36,5 +43,63 @@ namespace SecretSanta.Controllers
 
             return View("AssignmentCompleted", assignments);
         }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult SendAssignmentEmails()
+        {
+            if (!_userRepository.WasAssigned())
+                return RedirectToAction("Index");
+
+            var assignments = _userRepository.GetAssignments();
+            var assignmentModels = new ConcurrentBag<AssignmentEmailViewModel>();
+            var abandonedModels = new ConcurrentBag<AbandonedEmailViewModel>();
+            var parallelOptions = new ParallelOptions
+            {
+                MaxDegreeOfParallelism = 5
+            };
+            Parallel.ForEach(assignments.Assignments,parallelOptions,
+                assignment =>
+                {
+                    var sender = assignments.UserDisplayById[assignment.GiverId];
+                    var target = assignments.UserDisplayById[assignment.RecepientId];
+
+                    var success = _emailService.SendAssignmentEmail(sender, target);
+                    
+                        assignmentModels.Add(new AssignmentEmailViewModel
+                        {
+                            SenderDisplayName = sender.DisplayName,
+                            SenderEmail = sender.Email,
+                            TargetDisplayName = target.DisplayName,
+                            TargetEmail = target.Email,
+                            Success = success
+                        });
+                    
+                });
+            Parallel.ForEach(assignments.Abandoned, parallelOptions,
+                abandoned =>
+                {
+                    var user = assignments.UserDisplayById[abandoned.SantaUserId];
+                    var success = _emailService.SendAbandonmentEmail(user,
+                        abandoned.Reason);
+                    abandonedModels.Add(new AbandonedEmailViewModel
+                    {
+                        DisplayName = user.DisplayName,
+                        Email = user.Email,
+                        Reason = abandoned.Reason,
+                        Success = success
+                    });
+                    
+                });
+
+            var model = new SendAssignmentEmailsViewModel
+            {
+                Assignments = assignmentModels.ToArray(),
+                Abandonments = abandonedModels.ToArray()
+            };
+
+            return View(model);
+        }
+
     }
 }
---
+++ SecretSanta/Helpers/AbandonmentReasonHelper.cs
@@ -0,0 +1,13 @@
+﻿using SecretSanta.Domain.Enums;
+
+namespace SecretSanta.Helpers
+{
+    public static class AbandonmentReasonHelper
+    {
+        public static string GetUserFriendlyDescription(this AbandonmentReason reason)
+        {
+            return Resources.Global.ResourceManager.GetString($"AbandonmentReason_Text_{reason.ToString()}") ??
+                   Resources.Global.AbandonmentReason_Unknown;
+        }
+    }
+}
